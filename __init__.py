76ddd5c36e9691a9282b4ca479cd096a5aff646d
Alessandro Serra <a.serra132@studenti.unica.it>
2026-02-04 12:07:54 +0100

    exposed all function in the __init__.py file

--- __init__.py
+++ __init__.py
@@ -3,12 +3,25 @@ from . import (
     lmp2gpumd,
     spectral,
     temps,
+    temps_numba_parallel,
     unwrap_coords,
     wignerEXC,
     wignerEXC2B,
+    rdf,
+    exyz2gpumd,
+    writeGPUMDdump,
 )
 
+# Expose main functions for better IntelliSense
+from .temps import analyzeTEMPS
+from .rdf import compute_rdf, compute_R1, compute_I2, propagate_R1_error, propagate_I2_error
+from .spectral import calculateVACFgroup, calculateVDOS
+from .unwrap_coords import unwrap_coords as unwrap_coordinates
+from .exyz2gpumd import read_exyz, write_gpumd
+from .writeGPUMDdump import writeXYZ
+
 __all__ = [
+    # Modules
     "spectral",
     "unwrap_coords",
     "wignerEXC",
@@ -16,4 +29,21 @@ __all__ = [
     "lmp2gpumd",
     "c_parser",
     "temps",
+    "temps_numba_parallel",
+    "rdf",
+    "exyz2gpumd",
+    "writeGPUMDdump",
+    # Functions
+    "analyzeTEMPS",
+    "compute_rdf",
+    "compute_R1",
+    "compute_I2",
+    "propagate_R1_error",
+    "propagate_I2_error",
+    "calculateVACFgroup",
+    "calculateVDOS",
+    "unwrap_coordinates",
+    "read_exyz",
+    "write_gpumd",
+    "writeXYZ",
 ]
